47be53652176fdf4180825a92001de9b5e0e7841
Marjona20 <92976689+Marjona20@users.noreply.github.com>
2022-01-16 19:39:12 +0500

    Add files via upload

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,45 +1,72 @@
-import React from "react";
+import React, { useState } from 'react';
+import logo from '../../assets/images/logo-1.png';
+import { Anchor, Drawer, Button } from 'antd';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './header.css'
-import  "../Assets/images/fon1.jpg"
-import logo from "../Assets/images/logoo1.png"
-import Nav from 'react-bootstrap/Nav'
 
-const Header = () => {
-    return (
-   <div className="header pt-3">
+const { Link } = Anchor;
 
-<Nav defaultActiveKey="/home" as="ul" className="container">
-<Nav.Item as="li">
-    <Nav.Link href="/home"><img src={logo} class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt=""></img></Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link href="/home">Darslar</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link >Topik</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link >Konsalting</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link >Kitoblar</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link >To’lov</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li">
-    <Nav.Link >Biz haqimizda</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li" className="ms-5 ps-5">
-    <Nav.Link>Kirish</Nav.Link>
-  </Nav.Item>
-  <Nav.Item as="li" className="btn btn-light">
-    <Nav.Link>Registratsiya</Nav.Link>
-  </Nav.Item>
-</Nav>    
+function AppHeader() {
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="header">
+        <div className="mobileHidden">
+          <Anchor targetOffset="65">
+          <div className="me-5">
+          <img src={logo} style={{width:'130px', height:'40px'}}  alt=""></img>
+        </div>
+            <div className="h-div ms-5 me-5">
+            <Link href="/home" title="Darslar" />
+            <Link href="/home" title="Topik" />
+            <Link href="/home" title="Konsalting" />
+            <Link href="/home" title="Kitoblar" />
+            <Link href="/home" title="To'lov" />
+            <Link href="/home" title="Biz haqimizda" />
+            </div>
+            <div className="h-div ms-5">
+            <Link href="/home" title="Kirish" />
+            <Button className="btn-secondary"><Link href="/home" title="Registratsiya" /></Button>
+          </div>
+          </Anchor>
+        </div>
+        <div className="mobileVisible">
+        <div className="d-flex fa-align-center">
+         <Button className="me-5" type="primary" onClick={showDrawer}>
+            <i className="fas fa-bars"></i>
+          </Button>
+          <img className="ms-5 me-5" src={logo} style={{width:'150px', height:'50px'}}  alt="logo"></img>
+           <div className="restangle-1 px-5 text-1">
+           <h3 className="text-1 mt-2">Kabinet</h3>
+           </div>
+          </div>
+          <Drawer
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor targetOffset="65">
+            <Link href="/home" title="Darslar" />
+            <Link href="/home" title="Topik" />
+            <Link href="/home" title="Konsalting" />
+            <Link href="/home" title="Kitoblar" />
+            <Link href="/home" title="To'lov" />
+            <Link href="/home" title="Biz haqimizda" />
+            </Anchor>
+          </Drawer>
+        </div>
       </div>
-    )
+    </div>
+  );
 }
 
-export default Header;
+export default AppHeader;
