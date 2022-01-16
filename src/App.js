9796fe20f51ae4110a7d30acf0ed03907bbb554a
Marjona20 <92976689+Marjona20@users.noreply.github.com>
2022-01-16 19:36:22 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from "./Header/header";
 import MainSection from "./Main/MainSection";
-import Tests from "./Section2/section2";
+import AppSection2 from "./Section2/section2";
 import Solution from "./Solution";
 
 
 function App() {
   return (
     <div>
-      {/* <Solution />
-      <Tests /> */}
+      
     <Header />
+    <AppSection2 />
     <MainSection />
     </div>
   );
