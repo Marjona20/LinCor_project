04310cf2fbb6f8e6e8c4f6e784cdb86db7730f77
Marjona20 <92976689+Marjona20@users.noreply.github.com>
2022-01-16 19:47:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./Header/header";
 import MainSection from "./Main/MainSection";
+import AppSection1 from "./Section2/section1";
 import AppSection2 from "./Section2/section2";
 import Solution from "./Solution";
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       
     <Header />
+    <AppSection1 />
     <AppSection2 />
     <MainSection />
     </div>
