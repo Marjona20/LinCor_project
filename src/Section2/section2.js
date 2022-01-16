531938af517b81481a1211c9bccf4837b95ebcc8
Marjona20 <92976689+Marjona20@users.noreply.github.com>
2022-01-16 19:34:05 +0500

    Add files via upload

--- src/Section2/section2.js
+++ src/Section2/section2.js
@@ -1,27 +1,61 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './section2.css'
+import React from 'react';
 
-const massiv=[
-    {
-        name1:'초급',
-        text1:'Choose your doctor from thousands of specialist, general, and trusted hospitals'
-    },
-    {
-        name1:'중급',
-        text1:'Buy  your medicines with our mobile application with a simple delivery system'
-    },
-    {
-        name1:'고급',
-        text1:'Free consultation with our trusted doctors and get the best recomendations'
-    },
-]
-const Tests = () => {
-    return (
-<div className="section2 mt-5">
-  
-     </div>
-    )
+import { List, Card } from 'antd';
+
+const data = [
+  {
+        number:'01',
+        first:'초급',
+        daraja:"Boshlang'ich",
+        text:'Boshlang’ich darajadagilar uchun darslar to’plami. Umumiy darslar 25 tani tashkil qiladi.'
+  },
+      {
+        number:'02',
+        first:'중급',
+        daraja:"O’rta",
+        text:'Boshlang’ich darajadagilar uchun darslar to’plami. Umumiy darslar 25 tani tashkil qiladi.'
+      },
+      {
+        number:'03',
+        first:'고급 ',
+        daraja:"Yuqori",
+        text:'Boshlang’ich darajadagilar uchun darslar to’plami. Umumiy darslar 25 tani tashkil qiladi.'
+      }
+];
+
+function AppSection2() {
+  return (
+    <div id="pricing" className="block pricingBlock bgGray">
+      <div className="container-fluid">
+        <div className="titleHolder">
+          <h1 className="title-01">한국어 비디 <span className="text-danger">오</span> 수 <span className="text-danger">업</span> </h1>
+          <h2 className="title-01">Koreys tili video <span className="text-danger">darsliklar</span> to’plami</h2>
+          </div>
+        <List
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 1,
+            md: 3,
+            lg: 3,
+            xl: 3,
+            xxl: 3,
+          }}
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <Card className="px-5 card1">
+                <h1 className="text-num">{item.number}</h1>
+                <p className="text-num2"> {item.first} <span className="text-dark">{item.daraja}</span> daraja</p>
+                <p>{item.text}</p>
+                <hr className="hr-style"></hr>
+              </Card>
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>  
+  );
 }
 
-export default Tests;
+export default AppSection2;
